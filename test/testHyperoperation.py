ab2bf0b85ae0e86c62bddbdb9c3606c9ecdf4ef5
ymir <mazieres.pierre@gmail.com>
2017-12-07 15:27:15 +0100

    test addition surrogate from 1 to 10

--- test/testHyperoperation.py
+++ test/testHyperoperation.py
@@ -2,11 +2,18 @@
 # PY test script file name must start with "test" to allow automatic recognition by PyCharm
 # import
 import unittest
+from hyperoperation.hyperoperation import subAddition
 # define test
 class testHyperoperation(unittest.TestCase):
-    # test addition
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    # test addition surrogate
+    def testAdditionSurrogate(self):
+        for repertedNumber in range(1,10):
+            for repertingNumber in range(1, 10):
+                expectedAddition=repertedNumber+repertingNumber
+                actualAddition=subAddition(repertedNumber,0)
+                for repetition in range(1,repertingNumber):
+                    actualAddition=subAddition(actualAddition,repertedNumber)
+                self.assertEqual(expectedAddition,actualAddition,str(repertedNumber)+"+"+str(repertingNumber))
 # run test
 if __name__ == '__main__':
     unittest.main()
