8f1640e1b4cd7e3527d8381be3315e6f477c9720
ymir <mazieres.pierre@gmail.com>
2017-12-07 20:08:21 +0100

    add Jump option : smooth or rough

--- hyperoperation/hyperoperation.py
+++ hyperoperation/hyperoperation.py
@@ -1,20 +1,29 @@
 #!/usr/bin/env python3
 __version__ = '0.0.0'
-def subAddition(a,b):
+from enum import Enum, unique
+@unique
+class Jump(Enum):
+    smooth = True
+    rough = False
+'''
+TODO :
+ - define reverse operation
+ - define for decimal numbers
+ - define for negative numbers
+'''
+def subAddition(a,b,jump=Jump.smooth):
     # initiation
     result=None
     # do defined operation
     if a==b:
         result=2+a
-    else:
-        result = max(a, b) + 1
-    '''elif abs(a-b)>1:
+    elif jump.value or (not jump.value and abs(a-b)>1):
         result = max(a, b) + 1
     else:
-        result=max(a,b)'''
+        result=max(a,b)
     # finalisation
     return result
-def redifinedAddition(repertedNumber,repertingNumber):
+def redifinedAddition(repertedNumber,repertingNumber,jump=Jump.smooth):
     # initiation
     result=None
     # the sub addition is not defined for adding 1
@@ -24,6 +33,6 @@ def redifinedAddition(repertedNumber,repertingNumber):
     else:
         result = repertedNumber
         for repetition in range(1, repertingNumber):
-            result = subAddition(result, repertedNumber)
+            result = subAddition(result, repertedNumber,jump)
     # finalisation
     return result
--- test/testHyperoperation.py
+++ test/testHyperoperation.py
@@ -2,8 +2,14 @@
 # PY test script file name must start with "test" to allow automatic recognition by PyCharm
 # import
 import unittest
-from hyperoperation.hyperoperation import subAddition,redifinedAddition
+from hyperoperation.hyperoperation import subAddition,redifinedAddition,Jump
 # define test
+'''
+TODO :
+ - test bump
+ - test symetry
+ - test associativity
+'''
 class testHyperoperation(unittest.TestCase):
     # test redifined surrogate
     def testRedifinedAddition(self):
@@ -12,6 +18,14 @@ class testHyperoperation(unittest.TestCase):
                 expectedAddition=repertedNumber+repertingNumber
                 actualAddition=redifinedAddition(repertedNumber,repertingNumber)
                 self.assertEqual(expectedAddition,actualAddition,str(repertedNumber)+"+"+str(repertingNumber))
+    # test smooth
+    def testJump(self):
+        for number in range(1, 10):
+            smoothValue=subAddition(number,number-1)
+            roughValue=subAddition(number,number-1,Jump.rough)
+            associatedRoughValue=subAddition(number,number-1,Jump.rough)
+            self.assertEqual(smoothValue, roughValue+1, "Jump rough/smooth : "+str(smoothValue) + "+" + str(roughValue))
+            self.assertEqual(roughValue, associatedRoughValue, "Jump associated : " + str(roughValue) + "+" + str(associatedRoughValue))
 # run test
 if __name__ == '__main__':
     unittest.main()
