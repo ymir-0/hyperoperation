55f1ceabd18358293670d98294b89d0b3888f8d7
ymir <mazieres.pierre@gmail.com>
2017-12-07 17:01:06 +0100

    recreate addition for adding 1 exception

--- hyperoperation/hyperoperation.py
+++ hyperoperation/hyperoperation.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 __version__ = '0.0.0'
 def subAddition(a,b):
+    # initiation
     result=None
+    # do defined operation
     if a==b:
         result=2+a
     else:
@@ -10,4 +12,18 @@ def subAddition(a,b):
         result = max(a, b) + 1
     else:
         result=max(a,b)'''
+    # finalisation
+    return result
+def redifinedAddition(repertedNumber,repertingNumber):
+    # initiation
+    result=None
+    # the sub addition is not defined for adding 1
+    if repertedNumber == 1 or repertingNumber == 1:
+        result = repertedNumber+repertingNumber
+    # otherwise, addition can be descripted with sub addition
+    else:
+        result = repertedNumber
+        for repetition in range(1, repertingNumber):
+            result = subAddition(result, repertedNumber)
+    # finalisation
     return result
--- test/testHyperoperation.py
+++ test/testHyperoperation.py
@@ -2,20 +2,15 @@
 # PY test script file name must start with "test" to allow automatic recognition by PyCharm
 # import
 import unittest
-from hyperoperation.hyperoperation import subAddition
+from hyperoperation.hyperoperation import subAddition,redifinedAddition
 # define test
 class testHyperoperation(unittest.TestCase):
-    # test addition surrogate
-    def testAdditionSurrogate(self):
+    # test redifined surrogate
+    def testRedifinedAddition(self):
         for repertedNumber in range(0,10):
             for repertingNumber in range(0, 10):
                 expectedAddition=repertedNumber+repertingNumber
-                if repertedNumber==1 or repertingNumber==1:
-                    actualAddition=expectedAddition
-                else :
-                    actualAddition=repertedNumber
-                    for repetition in range(1,repertingNumber):
-                         actualAddition=subAddition(actualAddition,repertedNumber)
+                actualAddition=redifinedAddition(repertedNumber,repertingNumber)
                 self.assertEqual(expectedAddition,actualAddition,str(repertedNumber)+"+"+str(repertingNumber))
 # run test
 if __name__ == '__main__':
